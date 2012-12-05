81fa6740c8c1c0d82fe6d0098df8d15fad0bbdb3
Jesse Tane <jesse.tane@gmail.com>
2012-12-04 20:48:12 -0500

    only use semver version matching as a fallback

--- bin/portland.js
+++ bin/portland.js
@@ -6,9 +6,9 @@
  */
 
 
-var cui = require("cui");
-var portland = require("../");
-var args = process.argv.slice(2);
+var cui = require("cui")
+var portland = require("../")
+var args = process.argv.slice(2)
 
 cui.push({
   title: "choose an action:",
@@ -16,41 +16,45 @@ cui.push({
   data: Object.keys(require("../lib/registry")),
   action: function (cb) {
     if (args.length === 1 && args[0] === "lookup") {
-      cui.results.push("");
+      cui.results.push("")
     } else {
-      cui.push(services);
+      cui.push(services)
     }
-    cui.push(perform);
-    cb();
+    cui.push(perform)
+    cb()
   }
-});
+})
 
 var services = {
   type: "fields",
   data: [ "type a service name: " ]
-};
+}
 
 function perform (cb) {
-  var action = cui.last(2);
-  var service = cui.last(1);
+  var action = cui.last(2)
+  var service = cui.last(1)
   portland[action](service, function (err, resp) {
-    if (action === "register") {
-      if (resp) {
-        console.log(resp.port);
-      }
-    } else if (action === "lookup") {
-      if (resp.length === 0) {
-        err = new Error();
-      } else if (service) {
-        console.log(resp[0].port);
-      } else {
-        for (var s in resp) {
-          service = resp[s];
-          console.log(service.host + ":" + service.port);
+    if (err) {
+      console.error(err.message)
+    } else {
+      if (action === "register") {
+        if (resp) {
+          console.log(resp.port)
+        }
+      } else if (action === "lookup") {
+        if (resp.length === 0) {
+          err = new Error()
+        } else if (service) {
+          console.log(resp[0].port)
+        } else {
+          for (var s in resp) {
+            service = resp[s]
+            console.log(service.host + ":" + service.port)
+          }
         }
       }
     }
-    cb();
-    process.exit(err ? 1 : 0);
-  });
+    cb()
+    process.exit(err ? 1 : 0)
+  })
 }
